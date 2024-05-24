0c29cada72922c24f4085d160d1991c1cdab5a32
KIMMINJU <alswnkmj@g.skku.edu>
2024-05-25 01:43:45 +0900

    stopwatch

--- watch.srcs/sources_1/new/stopwatch.v
+++ watch.srcs/sources_1/new/stopwatch.v
@@ -20,51 +20,53 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module stopwatch(input clk, sw_rstb, [31:0] ms_acc, [5:0] prst, sw_lap, output reg [5:0] sw_sec, output reg [5:0] sw_min, output reg [5:0] sw_hr, output reg [5:0] sw_sec_lap, output reg [5:0] sw_min_lap, output reg [4:0] sw_hr_lap);
 
 
+module stopwatch(
+input wire clk, rstb, sw_lap, sw_stop, sw_start, 
+input wire [15:0] dip_switch,
+input wire [4:0] push_switch,
 
-	always@(posedge clk)
-	begin
-		if(sw_rstb==1'b0)
-		begin
-		   sw_sec=prst;
-		   sw_min=prst;
-		   sw_hr=prst; 
-		end
-		else 
-		begin
-		   if (sw_lap==1'b1) //record time 
-		   begin
-		       sw_sec_lap <= sw_sec;
-		       sw_min_lap <= sw_min;
-		       sw_hr_lap <= sw_hr;
-		   end
-		   else
-		   begin
-		       if (ms_acc>=0  && ms_acc%4'd1000==0)
-		       begin
-		           if (sw_sec == 6'b111100)
-		              sw_sec <= 0;
-		           else 
-		           begin
-		              sw_sec <= sw_sec+1;
-		           end
-		           if (ms_acc>=16'd60000 && ms_acc % 16'd60000 == 0)
-		           begin
-		              if (sw_min == 6'b111100)
-		                 sw_min <= 0;
-		              else 
-		              begin 
-		                 sw_min <= sw_min+1;
-		              end
-		              if (ms_acc>=22'd3600000 && ms_acc % 22'd3600000 == 0)
-		              begin
-		                 sw_hr <= sw_hr+1;
-		              end
-		           end   
-		       end               
-		   end
-		end   
-	end
-endmodule
+output wire [7:0] seg_data,
+output wire [7:0] digit,
+output reg [7:0] sw_lap_time,
+output reg [7:0] sw_stop_time,
+output reg sw_start_display,
+output wire [15:0] led,
+output reg [7:0] sw_time);
+
+reg [7:0] current_time_reg;
+
+
+clock_counter clock_counter(.clk(clk), .rstb(rstb), .dip_switch(dip_switch), .push_switch(push_switch), .seg_data(seg_data), .digit(digit), .led(led));
+
+always @(posedge clk or negedge rstb) begin
+  if (!rstb) 
+    begin
+      sw_lap_time <= 8'b0;
+      sw_stop_time <= 8'b0;
+      sw_start_display <= 1'b0;
+      sw_time <= 8'b0;
+      current_time_reg <= 8'b0;
+    end
+  else 
+    begin
+      if (sw_lap == 1'b1) 
+        begin 
+          sw_lap_time <= current_time_reg;
+        end
+      if (sw_stop == 1'b1)
+        begin 
+          sw_stop_time <= current_time_reg;
+          sw_start_display <= 1'b0;
+        end 
+      if (sw_start == 1'b1)
+        begin 
+          sw_start_display <= 1'b1; 
+          current_time_reg <= sw_stop_time+1;
+        end
+      sw_time <= current_time_reg;
+    end 
+end
+
+endmodule 
