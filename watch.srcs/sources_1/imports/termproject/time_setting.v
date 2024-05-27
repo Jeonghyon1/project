56891e61720a4b4c266455ba1f0001405dbbfc8b
mirinae3145 <mirinae3145@g.skku.edu>
2024-05-27 17:39:16 +0900

    Update time_setting.v

--- watch.srcs/sources_1/imports/termproject/time_setting.v
+++ watch.srcs/sources_1/imports/termproject/time_setting.v
@@ -1,60 +1,57 @@
 module time_setting (
-    input wire clk,
-    input wire rstb,
-    input wire [7:0] dip_switch,
-    input wire [4:0] push_switch,
-    output reg [31:0] time_value,
-    output reg set_time_mode
+    input clk,
+    input rstb,
+    input [5:0] digits,
+    input inc,
+    input dec,
+    input mode, //true for date, false for time
+    output [41:0] t
 );
+reg [3:0] val_y [5:0];
+reg [3:0] val_t [5:0];
+integer i,n;
 
-// Internal time registers
-reg [3:0] month_tens, month_units, day_tens, day_units;
-reg [3:0] hour_tens, hour_units, minute_tens, minute_units, second_tens, second_units;
+function [6:0] d2b(input [3:0] tens,units);
+    d2b = 10 * tens + units;
+endfunction
+
+reg [5:0] yr,mon,day,hr,min,sec;
+assign t={1970+yr,mon,day,hr,min,sec};
 
 always @(posedge clk) begin
     if (!rstb) begin
-        month_tens <= 0;
-        month_units <= 0;
-        day_tens <= 0;
-        day_units <= 0;
-        hour_tens <= 0;
-        hour_units <= 0;
-        minute_tens <= 0;
-        minute_units <= 0;
-        second_tens <= 0;
-        second_units <= 0;
-    end else begin
-        if (dip_switch[0]) begin
-            if (push_switch[0]) month_tens <= (month_tens < 9) ? month_tens + 1 : month_tens;
-            if (push_switch[1]) month_tens <= (month_tens > 0) ? month_tens - 1 : month_tens;
-        end else if (dip_switch[1]) begin
-            if (push_switch[0]) month_units <= (month_units < 9) ? month_units + 1 : month_units;
-            if (push_switch[1]) month_units <= (month_units > 0) ? month_units - 1 : month_units;
-        end else if (dip_switch[2]) begin
-            if (push_switch[0]) day_tens <= (day_tens < 9) ? day_tens + 1 : day_tens;
-            if (push_switch[1]) day_tens <= (day_tens > 0) ? day_tens - 1 : day_tens;
-        end else if (dip_switch[3]) begin
-            if (push_switch[0]) day_units <= (day_units < 9) ? day_units + 1 : day_units;
-            if (push_switch[1]) day_units <= (day_units > 0) ? day_units - 1 : day_units;
-        end else if (dip_switch[4]) begin
-            if (push_switch[0]) hour_tens <= (hour_tens < 9) ? hour_tens + 1 : hour_tens;
-            if (push_switch[1]) hour_tens <= (hour_tens > 0) ? hour_tens - 1 : hour_tens;
-        end else if (dip_switch[5]) begin
-            if (push_switch[0]) hour_units <= (hour_units < 9) ? hour_units + 1 : hour_units;
-            if (push_switch[1]) hour_units <= (hour_units > 0) ? hour_units - 1 : hour_units;
-        end else if (dip_switch[6]) begin
-            if (push_switch[0]) minute_tens <= (minute_tens < 9) ? minute_tens + 1 : minute_tens;
-            if (push_switch[1]) minute_tens <= (minute_tens > 0) ? minute_tens - 1 : minute_tens;
-        end else if (dip_switch[7]) begin
-            if (push_switch[0]) minute_units <= (minute_units < 9) ? minute_units + 1 : minute_units;
-            if (push_switch[1]) minute_units <= (minute_units > 0) ? minute_units - 1 : minute_units;
-        end
-
-        if (push_switch[2]) begin
-            set_time_mode <= 1;
-            time_value <= {month_tens, month_units, day_tens, day_units, hour_tens, hour_units, minute_tens, minute_units};
+        for(i=0;i<6;i=i+1) begin
+            val_y[i]=0;
+            val_t[i]=0;
         end
     end
+    
+    n=6;
+    for(i=0;i<6;i=i+1) begin
+        n<=i*digits[i];
+    end
+    
+    yr<=d2b(val_y[5],val_y[4]);
+    mon<=d2b(val_y[3],val_y[2]);
+    day<=d2b(val_y[1],val_y[0]);
+    
+    hr<=d2b(val_t[5],val_t[4]);
+    min<=d2b(val_t[3],val_t[2]);
+    sec<=d2b(val_t[1],val_t[0]);
+    
+    
 end
+
+always@(posedge inc)
+    if(mode)
+        val_y[n]<=val_y[n]+1;
+    else
+        val_t[n]<=val_t[n]+1;
+        
+always@(posedge dec)
+    if(mode)
+        val_y[n]<=val_y[n]-1;
+    else
+        val_t[n]<=val_t[n]-1;
 
 endmodule
